a4ab48bfc47995c0f099874a65fcd6cbc84f5bee
DmiE <Dawid.Mieszczak95@gmail.com>
2020-02-19 20:54:01 +0100

    change right menu icons, add smooth scroll

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// SMOOTH SCROLL
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
