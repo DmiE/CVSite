44ad07b581c32474358e412cb718b20b67d583e0
DmiE <Dawid.Mieszczak95@gmail.com>
2020-06-24 20:28:47 +0200

    add form validation

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+//CONTACT FORM
+
+const form = document.getElementById('contact_form');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+const validate = input => {
+  if (input.type === 'email') {
+    console.log('email validation');
+  } else if (input.type === 'textarea') {
+    console.log('text validation');
+  }
+};
+
+const showError = () => {
+  console.log('show error');
+};
+
+form.addEventListener('submit', event => {
+  validate(email);
+  validate(message);
+
+  if (true) {
+    // showError();
+    event.preventDefault();
+  }
+});
