eb50a1dec8dc709d0ff45d4b3b15caed00ac8a99
DmiE <Dawid.Mieszczak95@gmail.com>
2020-06-24 23:36:57 +0200

    add email validation function

--- src/js/index.js
+++ src/js/index.js
@@ -16,21 +16,50 @@ const form = document.getElementById('contact_form');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
 
-const validate = input => {
-  if (input.type === 'email') {
-    console.log('email validation');
-  } else if (input.type === 'textarea') {
-    console.log('text validation');
+const validateEmail = input => {
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (input.value === '') {
+    showError('Field is empty!');
+    return false;
+  } else if (!emailRegex.test(String(input.value).toLowerCase())) {
+    showError('Incorrect email address!');
+    return false;
+  } else if (input.value.length > 150) {
+    showError('Your email address is too long');
+    return false;
+  } else {
+    return true;
   }
 };
 
-const showError = () => {
-  console.log('show error');
+const validateTextField = (input, maxLength) => {
+  if (input.value === '') {
+    showError('Field is empty!');
+    return false;
+  } else if (input.value >= maxLength) {
+    showError('Your message is too long');
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const showError = message => {
+  console.log(message);
+};
+
+const hideError = () => {
+  console.log('error hidden');
 };
 
 form.addEventListener('submit', event => {
-  validate(email);
-  validate(message);
+  validateEmail(email);
+  validateTextField(message);
+
+  if (!validateEmail(email) || !validateTextField(message)) {
+    console.log('walidacja nie przeszła');
+  }
 
   if (true) {
     // showError();
