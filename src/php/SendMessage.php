0e12bc3504e57e57c5273e429483f12dcb45970e
DmiE <Dawid.Mieszczak95@gmail.com>
2020-08-18 16:11:45 +0200

    add php validation

--- src/php/SendMessage.php
+++ src/php/SendMessage.php
@@ -1,36 +1,63 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
+// require __DIR__ . '/vendor/autoload.php';
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+$errors = [];
+$errorMessage = '';
 
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-require 'Exception.php';
-require 'PHPMailer.php';
-require 'SMTP.php';
-$mail = new PHPMailer();
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->CharSet = "UTF-8";
-$mail->PluginDir = "phpmailer/";
-$mail->From = "dmiecvsite@gmail.com";
-$mail->FromName = "Dawid Mieszczak";
-$mail->Host = "smtp.gmail.com";
-$mail->Mailer = "smtp";
-$mail->Username = process.env.MAIL_USERNAME; //username
-$mail->Password = process.env.MAIL_PASSOWRD; //password
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'ssl'; 
-$mail->Port = 465; //port poczty
-$mail->SetLanguage("pl", "phpmailer/language/");
-$mail->Subject = "CVSite message"; //temat wiadomości
-$mail->Body = 'Wiadomość od : ' . $email . ',
-Treść wiadomości: ' . $message . '.'; //wiadomość
-$mail->AddAddress("dmiecvsite@gmail.com", "Dawid Mieszczak"); //do kogo ma być wysłana
-if ($mail->Send()) {
+if (empty($email)) {
+    $errors[] = 'Email is empty';
+} else if (!filter_var($email, FILTER_VALIDATE_REGEXP, array(
+    "options" => array("regexp"=>'/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/')
+))) {
+    $errors[] = 'Email is invalid';
+}
+
+if (empty($message)) {
+    $errors[] = 'Message is empty';
+}
+
+if (!empty($errors)) {
+    $allErrors = join('<br/>', $errors);
+    $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    echo $errorMessage;
+} else {
+    $mail = new PHPMailer();
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->CharSet = "UTF-8";
+    $mail->PluginDir = "phpmailer/";
+    $mail->From = $email;
+    $mail->FromName = "Dawid Mieszczak CV Site form";
+    $mail->Host = "smtp.gmail.com";
+    $mail->Mailer = "smtp";
+    $mail->Username = "-"; //username
+    $mail->Password = "-"; //password    
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'ssl'; 
+    $mail->Port = 465; //port poczty
+    $mail->SetLanguage("pl", "phpmailer/language/");
+    $mail->Subject = "CVSite message"; //temat wiadomości
+    $mail->Body = 'Wiadomość od : ' . $email . ',
+    Treść wiadomości: ' . $message . '.'; //wiadomość
+    $mail->AddAddress("dmiecvsite@gmail.com", "Dawid Mieszczak"); //do kogo ma być wysłana
+
+    if($mail->send()){
+        // header('Location: thank-you.html'); // redirect to 'thank you' page
         echo 'ok'; //odpowiedź
     } else {
-        echo $mail->ErrorInfo;
+        $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $mail->ErrorInfo;
+        echo $errorMessage;
     }
-$mail->ClearAddresses();
-$mail->ClearAttachments();
- 
+
+    $mail->ClearAddresses();
+    $mail->ClearAttachments();
+}
+?>
